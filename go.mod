9756451418fe699b161be1df5b7568d7c3a2e7df
Chris H <squid@sirsquidness.com>
2022-04-18 19:32:20 +1000

    Make race detector not detect race (#11)

--- go.mod
+++ go.mod
@@ -5,11 +5,15 @@ go 1.17
 require github.com/hajimehoshi/ebiten/v2 v2.2.7
 
 require (
+	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20210727001814-0db043d8d5be // indirect
 	github.com/jezek/xgb v0.0.0-20210312150743-0e0f116e1240 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.7.1 // indirect
 	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
 	golang.org/x/image v0.0.0-20210628002857-a66eb6448b8d // indirect
 	golang.org/x/mobile v0.0.0-20210902104108-5d9a33257ab5 // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
--- slippymap/osmtileprovider_test.go
+++ slippymap/osmtileprovider_test.go
@@ -1,53 +1,36 @@
 package slippymap
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
 
 func TestGetTileAddress(t *testing.T) {
 
-	TileProvider := &OSMTileProvider{}
+	t.Run("GetTileAddress loops through CDNs correctly", func(t *testing.T) {
+		// -1 so that the first increment lands us up on CDN A. Doesn't really matter where it starts though
+		TileProvider := &OSMTileProvider{osm_url_prefix: -1}
 
-	// check correct a... url is returned
-	url, err := TileProvider.GetTileAddress(1, 2, 3)
-	if err != nil {
-		t.Error(err)
-	}
-	if url != "http://a.tile.openstreetmap.org/3/1/2.png" {
-		t.Errorf("Expected http://a.tile.openstreetmap.org/3/1/2.png, got: %s", url)
-	}
+		// check correct a... url is returned
+		url, err := TileProvider.GetTileAddress(1, 2, 3)
+		require.NoError(t, err)
+		assert.Equal(t, "http://a.tile.openstreetmap.org/3/1/2.png", url)
 
-	// check correct b... url is returned
-	url, err = TileProvider.GetTileAddress(1, 2, 3)
-	if err != nil {
-		t.Error(err)
-	}
-	if url != "http://b.tile.openstreetmap.org/3/1/2.png" {
-		t.Errorf("Expected http://b.tile.openstreetmap.org/3/1/2.png, got: %s", url)
-	}
+		// check correct b... url is returned
+		url, err = TileProvider.GetTileAddress(1, 2, 3)
+		require.NoError(t, err)
+		assert.Equal(t, "http://b.tile.openstreetmap.org/3/1/2.png", url)
 
-	// check correct c... url is returned
-	url, err = TileProvider.GetTileAddress(1, 2, 3)
-	if err != nil {
-		t.Error(err)
-	}
-	if url != "http://c.tile.openstreetmap.org/3/1/2.png" {
-		t.Errorf("Expected http://c.tile.openstreetmap.org/3/1/2.png, got: %s", url)
-	}
+		// check correct c... url is returned
+		url, err = TileProvider.GetTileAddress(1, 2, 3)
+		require.NoError(t, err)
+		assert.Equal(t, "http://c.tile.openstreetmap.org/3/1/2.png", url)
 
-	// check correct a... url is returned (it loops back to a after c)
-	url, err = TileProvider.GetTileAddress(1, 2, 3)
-	if err != nil {
-		t.Error(err)
-	}
-	if url != "http://a.tile.openstreetmap.org/3/1/2.png" {
-		t.Errorf("Expected http://a.tile.openstreetmap.org/3/1/2.png, got: %s", url)
-	}
-
-	// set invalid tile prefix to generate an error
-	TileProvider.osm_url_prefix = 4
-	url, err = TileProvider.GetTileAddress(1, 2, 3)
-	if err != nil {
-		// test passes
-	} else {
-		t.Error("Expected an error, got none.")
-	}
+		// check correct a... url is returned (it loops back to a after c)
+		url, err = TileProvider.GetTileAddress(1, 2, 3)
+		require.NoError(t, err)
+		assert.Equal(t, "http://a.tile.openstreetmap.org/3/1/2.png", url)
+	})
 }
