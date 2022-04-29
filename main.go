d141709aae10dd2e8b0d53025a57433a847d4e2f
Mike Nye <mike.nye@gmail.com>
2022-04-29 18:24:35 +0800

    Add B77W (Boeing 777-300ER)

--- main.go
+++ main.go
@@ -298,6 +298,11 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	screen.DrawImage(m.Img, &do)
 	ebitenutil.DebugPrintAt(screen, "B738", 550, 40)
 
+	m = (*g.aircraftMarkers)["B77W"]
+	do = m.MarkerDrawOpts(dbgMarkerRotateAngle, 600, 5)
+	screen.DrawImage(m.Img, &do)
+	ebitenutil.DebugPrintAt(screen, "B77W", 600, 40)
+
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
