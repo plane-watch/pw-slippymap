a785afeea0e7d0a27cd69eedd1f874db2a79b596
Mike Nye <mike.nye@gmail.com>
2022-05-08 22:26:15 +0800

    fix test

--- markers/markers_test.go
+++ markers/markers_test.go
@@ -1,29 +1,18 @@
 package markers
 
 import (
-	"fmt"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestMarkers(t *testing.T) {
-	markers, err := InitMarkers()
-	require.NoError(t, err)
-
-	for _, m := range markers {
-		testName := fmt.Sprintf("PointInsideMarker: %s", m.icao)
-		t.Run(testName, func(t *testing.T) {
-			p := []float64{m.CentreX, m.CentreY}
-			isInside := m.PointInsideMarker(p[0], p[1])
-			assert.True(t, isInside)
-
-			p = []float64{0, 0}
-			isInside = m.PointInsideMarker(p[0], p[1])
-			assert.False(t, isInside)
-
-		})
-
-	}
+	t.Run("Aircraft", func(t *testing.T) {
+		_, err := InitMarkers(Aircraft)
+		require.NoError(t, err)
+	})
+	t.Run("GroundVehicles", func(t *testing.T) {
+		_, err := InitMarkers(GroundVehicles)
+		require.NoError(t, err)
+	})
 }
